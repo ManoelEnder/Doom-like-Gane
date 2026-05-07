9aff350f7484dff4e89c0604538c720b20d6ab77
MglBp <miguel.prestes@Alunos.sc.senac.br>
2026-05-07 09:03:50 -0300

    chosescene

---
+++ Doom Like Game/Assets/Miguel/Script/TrocaCena.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Essencial para gerenciar cenas
+
+public class TrocaCena : MonoBehaviour
+{
+    // Método público para ser visível no botão
+    public void CarregarCena(string nomeDaCena)
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+}
